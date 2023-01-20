2d7c74e984dddbc65eeeba83e7ab07be3c5d7306
shahriar <shahriarksaurov@gmail.com>
2023-01-21 01:38:20 +0200

    api call realtime data

--- weatherApp/src/app/app.component.ts
+++ weatherApp/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faTemperatureLow } from '@fortawesome/free-solid-svg-icons';
+import { tap } from 'rxjs';
+import { WeatherService } from './services/weather.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(
+    private weatherService: WeatherService
+  ){}
+  ngOnInit(): void {
+    // this.weatherService.getWeatherData('Helsinki').subscribe({
+    //   next:(response)=>{
+    //     console.log(response);
+        
+    //   }
+    // })
+    this.weatherService.getWeatherData('helsinki').pipe(
+      tap((res: any)=>console.log(res)
+      )
+    ).subscribe()
+  }
   title = 'weatherApp';
   faTemperatureLow = faTemperatureLow;
 }
---
+++ weatherApp/src/assets/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environment ={
+    production: false,
+    weatherApiBaseUrl:'https://weatherapi-com.p.rapidapi.com/current.json',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'09cfa80fdfmshfab9bb2e6524034p10409ejsn8327b13fb216'
+}   
